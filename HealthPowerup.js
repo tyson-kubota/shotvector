4ac44f2d1ec5f5d011c0eb1cd78d54cb9496bef6
tyson-kubota <tyson.kubota.web@gmail.com>
2014-03-26 19:11:13 -0400

    health powerup particle system
    can be generalized to other kinds of powerups too.

--- HealthPowerup.js
+++ HealthPowerup.js
@@ -1,6 +1,7 @@
 ﻿#pragma strict
 
 var nutrition : int = 5;
+var particleDuration : float = 4.0;
 
 function Start () {
 }
@@ -11,6 +12,16 @@ function Update () {
 function OnTriggerEnter (other : Collider) {
 	if (other.gameObject.tag == "Player") {
 		if (animation) {animation.Play();}
+		if (transform.particleSystem) {
+			Particles(transform.particleSystem); 
+			transform.renderer.enabled = false;
+		}
 	}
 
+}
+
+function Particles ( myParticles : ParticleSystem ) {
+	myParticles.Play();
+	yield WaitForSeconds(particleDuration);
+	myParticles.Stop();
 }
