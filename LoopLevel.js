33d7fcc040b67c948d6bc49fc022b5cee0f65f44
tyson-kubota <tyson.kubota.web@gmail.com>
2014-06-30 00:05:16 -0400

    add very primitive level chunk looping

---
+++ LoopLevel.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var myPoolObject : PoolObject;
+var myObject : GameObject;
+var MyLayerMask : int;
+var raycastDepth : int = 5000;
+var nextChunkObj : GameObject;
+var nextChunk : LoopedLevelChunk;
+var prevChunk : LoopedLevelChunk;
+
+function Start () {
+    MyLayerMask = (1 << LayerMask.NameToLayer("LevelChunkLayer"));
+}
+
+function OnTriggerEnter (other : Collider) {
+    // Instantiate(nextChunk, Vector3(other.gameObject.transform.position.x, 0, 0), Quaternion.identity);
+    Debug.Log("you hit the divider!");
+    //Instantiate(nextChunk, Vector3(transform.position.x, 0, 0), Quaternion.identity);
+    var spawnedChunk : GameObject = PoolManager.Spawn(nextChunkObj.name);
+    spawnedChunk.transform.position = Vector3(transform.position.x, 0, 0);
+    spawnedChunk.transform.rotation = Quaternion.identity;
+
+    //to detach from parent, once positioned
+    spawnedChunk.transform.parent = null;
+}
+
+function OnTriggerExit (other : Collider) {
+    prevChunk = other.gameObject.GetComponent(LoopedLevelChunk);
+    if (prevChunk) {prevChunk.CanHide();}
+}
---
+++ LoopedLevelChunk.js
@@ -0,0 +1,25 @@
+#pragma strict
+
+var isSolid : boolean = false;
+var readyToHide : boolean = false;
+var myPoolObject : PoolObject;
+var myObject : GameObject;
+
+function OnBecameVisible () {
+    if (isSolid == false) {
+        isSolid = true;
+        myPoolObject = myObject.GetComponent(PoolObject);
+    }
+}
+
+function OnBecameInvisible () {
+    if (isSolid == true && readyToHide == true) {
+        isSolid = false;
+        Debug.Log("Level chunk " + myPoolObject.name + " is now invisible." );
+        PoolManager.Despawn(myPoolObject.gameObject);
+    }
+}
+
+function CanHide() {
+	readyToHide = true;
+}
