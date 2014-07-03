1051a4c007fd6490ae01e741df03ee7a5836a89c
tyson-kubota <tyson.kubota.web@gmail.com>
2014-07-02 23:43:01 -0400

    enemy spawning, first pass
    needs some kind of onDespawn/onSpawn initializer to reset their status
    on death.

---
+++ EnemySpawnPoint.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var EnemyObj : GameObject;
+var EnemyObj2 : GameObject;
+var hasSpawned : boolean = false;
+
+function Start () {
+    hasSpawned = false;
+}
+
+function OnTriggerEnter (other : Collider) {
+    // Instantiate(nextChunk, Vector3(other.gameObject.transform.position.x, 0, 0), Quaternion.identity);
+
+    //Instantiate(nextChunk, Vector3(transform.position.x, 0, 0), Quaternion.identity);
+    var EnemyToSpawn : GameObject = Random.value < .75 ? EnemyObj : EnemyObj2;
+    if (other.gameObject.tag == "Player" && hasSpawned == false) {
+
+        #if UNITY_EDITOR
+        Debug.Log("you hit the Enemy spawn point!");
+        #endif
+
+        var spawnedEnemy : GameObject = PoolManager.Spawn(EnemyToSpawn.name);
+        spawnedEnemy.transform.position = Vector3(transform.position.x, 0, 0);
+        //spawnedEnemy.transform.rotation = Quaternion.identity;
+
+        //to detach from parent, once positioned
+        spawnedEnemy.transform.parent = null;
+
+        hasSpawned = true;
+    }
+}
--- LoopLevel.js
+++ LoopLevel.js
@@ -15,18 +15,24 @@ function Start () {
 
 function OnTriggerEnter (other : Collider) {
     // Instantiate(nextChunk, Vector3(other.gameObject.transform.position.x, 0, 0), Quaternion.identity);
-    Debug.Log("you hit the level chunk-spawning divider!");
-    //Instantiate(nextChunk, Vector3(transform.position.x, 0, 0), Quaternion.identity);
-    var ChunkToSpawn : GameObject = Random.value < .5 ? nextChunkObj : nextChunkObj2;
-    var spawnedChunk : GameObject = PoolManager.Spawn(ChunkToSpawn.name);
-    spawnedChunk.transform.position = Vector3(transform.position.x, 0, 0);
-    spawnedChunk.transform.rotation = Quaternion.identity;
+    prevChunk = other.gameObject.GetComponent(LoopedLevelChunk);
 
-    //to detach from parent, once positioned
-    spawnedChunk.transform.parent = null;
+    if (other.gameObject.tag == "Divider") {
+
+        #if UNITY_EDITOR
+        Debug.Log("you hit the level chunk-spawning divider!");
+        #endif
+
+        var ChunkToSpawn : GameObject = Random.value < .5 ? nextChunkObj : nextChunkObj2;
+        var spawnedChunk : GameObject = PoolManager.Spawn(ChunkToSpawn.name);
+        spawnedChunk.transform.position = Vector3(transform.position.x, 0, 0);
+        spawnedChunk.transform.rotation = Quaternion.identity;
+
+        //to detach from parent, once positioned
+        spawnedChunk.transform.parent = null;
+    }
 }
 
 function OnTriggerExit (other : Collider) {
-    prevChunk = other.gameObject.GetComponent(LoopedLevelChunk);
     if (prevChunk) {prevChunk.CanHide();}
 }
